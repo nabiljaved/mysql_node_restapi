3d4f786958dd26e29c74e3385f8c1f339605a75d
nabiljaved <nabeeljaved2016@gmail.com>
2022-03-06 03:47:22 +0500

    remove error

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const userRouter = require("./api/users/user.router");
 app.use(express.json());
 
 app.use("/api/users", userRouter);
+app.use("/", (req,res)=> {
+    res.send("hello")
+});
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log("server up and running on PORT :", port);
